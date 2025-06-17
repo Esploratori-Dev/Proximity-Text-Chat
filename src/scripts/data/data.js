2684e3645e9eaef09f36c1451494c42f8abf02e8
InnateAlpaca <innatealpaca@esploratorismp.space>
2025-06-18 00:33:53 +0200

    update for mc 1.21.90

--- src/scripts/data/data.js
+++ src/scripts/data/data.js
@@ -6,7 +6,7 @@ export const settings = {
     admin_tag: "admin",
     ignore_tag: "proximity:god",  // players with this tag will be able to read all chat
     options_command: "esploratori:proximity_setup",
-    rank_template: "[$...t§r] <$u> $m", // %t tag, %u username, %n nametag, %m message
+    rank_template: "[$...t( )§r] <$u> $m", // %t tag, %u username, %n nametag, %m message
     name_template: "[$t§r] $n", // %t tag, %u username, %n nametag, %m message
     do_template: false,
     do_name_template: false,
